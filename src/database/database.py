0ec0df510274e1c1a5d4324ea6007ebc832714f6
KlaudiaSternik <52292889+KlaudiaSternik@users.noreply.github.com>
2021-05-17 15:17:21 +0100

    Update database.py

--- src/database/database.py
+++ src/database/database.py
@@ -5,20 +5,35 @@ import appdirs
 import sqlite3
 from sqlite3 import Error
 
-sql_create_user_table = """
-
-"""
-sql_create_log_table = """
-
-"""
-sql_create_log_user_table = """
-
-"""
-sql_create_shard_table = """
-
-"""
-tables = [sql_create_user_table, sql_create_shard_table, sql_create_log_table, sql_create_log_user_table]
-tables_name = ["", "", "", ""]
+sql_create_user_table = """CREATE TABLE user(
+gwaccount VARCHAR(30),
+discordid VARCHAR(30),
+token CHAR(36),
+CONSTRAINT comp_key PRIMARY KEY (gwaccount, discordid)
+);"""
+sql_create_evtc_table = """CREATE TABLE evtc(
+url VARCHAR(80) PRIMARY KEY,
+cm BOOL NOT NULL,
+success BOOL NOT NULL,
+timestamp DATETIME NOT NULL,
+bossname VARCHAR(80) NOT NULL
+);"""
+sql_create_evtc_user_table = """ CREATE TABLE evtcuser(
+evtcuserid INT AUTO_INCREMENT PRIMARY KEY,
+gwaccount VARCHAR(30) NOT NULL,
+evtc VARCHAR(80) NOT NULL,
+FOREIGN KEY (evtc) REFERENCES evtc(url)
+ );"""
+sql_create_shard_table = """CREATE TABLE shard(
+shardid INT PRIMARY KEY,
+guildid CHAR(36),
+token CHAR(36),
+motdchannel INT, 
+raidchannel INT,
+raidlogchannel INT 
+);"""
+tables = [sql_create_shard_table, sql_create_user_table, sql_create_evtc_table, sql_create_evtc_user_table]
+tables_name = ["shard", "user", "evtc", "evtcuser"]
 
 
 def get_file_path():
@@ -77,7 +92,8 @@ class Database:
         toggle = False
         try:
             c = self.conn.cursor()
-            c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name=:table_name;", {"table_name": name})
+            c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name=:table_name;",
+                      {"table_name": name})
             toggle = c.fetchone()[0] == 1
         except Error as e:
             print(e)
